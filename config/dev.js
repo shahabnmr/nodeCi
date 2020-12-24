cd41d6d878349d63c623bfb941c43dc434a8b520
ofk8vb <latios_gs@hotmail.com>
2020-12-24 17:34:16 +0300

    fixed mongo uri leak

--- config/dev.js
+++ config/dev.js
@@ -2,6 +2,6 @@ module.exports = {
   googleClientID:
     '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
   googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
-  mongoURI: 'mongodb://farq:farq@freecodecamp-shard-00-00.dl3kc.mongodb.net:27017,freecodecamp-shard-00-01.dl3kc.mongodb.net:27017,freecodecamp-shard-00-02.dl3kc.mongodb.net:27017/stephenNodeAdvance?ssl=true&replicaSet=atlas-9fo80a-shard-0&authSource=admin&retryWrites=true&w=majority',
+  mongoURI: 'removed',
   cookieKey: '123123123',
 };
