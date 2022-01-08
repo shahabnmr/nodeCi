a2f328b9b07841cb930e4b56093ba13c4b25f202
shahabnmr <88978966+shahabnmr@users.noreply.github.com>
2022-01-08 13:20:44 +0330

    Delete config directory

--- config/dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  googleClientID:
-    "1008610353674-gctm3fp15gbt1ea537868blqum41qafl.apps.googleusercontent.com",
-  googleClientSecret: "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",
-  mongoURI: "mongodb://localhost:27017/redis_catch",
-  cookieKey: "123123123",
-  redisUrl: "redis://127.0.0.1:6379",
-};
